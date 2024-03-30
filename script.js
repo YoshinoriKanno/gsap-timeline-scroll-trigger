427337274f428ad28b481460652a7c7ec7bf41f6
Yoshinori Kanno <yoshinori.kanno@me.com>
2024-03-30 17:25:12 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// use a script tag or an external JS file
+document.addEventListener("DOMContentLoaded", (event) => {
+  // gsap code here!
+
+  // アニメーションを適用する要素の設定
+  const items = document.querySelectorAll(".product-item");
+
+  // GSAPのタイムラインを作成
+  const timeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".product-list", // トリガーとなる要素
+      scrub: true, // スクロールに応じてアニメーション
+      pin: true, // トリガー要素を固定
+      pinSpacing: false, // 固定時のスペーシングを無効
+      start: "top top", // トリガーの開始条件
+      endTrigger: "#how-to", // アニメーション終了のトリガー要素
+      end: "top bottom" // 終了条件
+    }
+  });
+
+  // 各アイテムに対してアニメーションを適用
+  items.forEach((item, index) => {
+    if (index !== items.length - 1) { // 最後のアイテム以外に適用
+      // 初期状態を設定
+      gsap.set(item, { clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)" });
+
+      // タイムラインにアニメーションを追加
+      timeline.to(item, {
+        clipPath: "polygon(0 0, 100% 0, 100% 0%, 0 0%)", // クリップパスのアニメーション
+        duration: 1 // アニメーションの期間
+      });
+    }
+  });
+});
+
